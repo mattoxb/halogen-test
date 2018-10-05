644ee0e1f27ed5b073982cd5ce1d6c07d08b4a5e
Mattox Beckman <mattox@illinois.edu>
2018-10-05 15:09:32 -0500

    Added my components.

--- src/Component.purs
+++ src/Component.purs
@@ -3,14 +3,20 @@ module Component where
 import Prelude
 
 import Data.Maybe (Maybe(..))
+import Data.Number (fromString)
+import Math (pow)
 
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as P
 
-data Query a = ToggleState a
+data Query a = EnterInitial String a
+             | EnterFinal String a
 
-type State = { on :: Boolean }
+type State = { initial :: Number
+             , final :: Number
+             , adjusted :: Number }
 
 component :: forall m. H.Component HH.HTML Query Unit Void m
 component =
@@ -23,26 +29,44 @@ component =
   where
 
   initialState :: State
-  initialState = { on: false }
+  initialState = { initial: 0.0
+                 , final: 0.0
+                 , adjusted: 0.0 }
 
   render :: State -> H.ComponentHTML Query
   render state =
     HH.div_
-      [ HH.h1_
-          [ HH.text "Hello world!" ]
+      [ HH.h2_
+          [ HH.text "Refractometer Calculator" ]
       , HH.p_
-          [ HH.text "Why not toggle this button:" ]
-      , HH.button
-          [ HE.onClick (HE.input_ ToggleState) ]
-          [ HH.text
-              if not state.on
-              then "Don't push me"
-              else "I said don't push me!"
-          ]
+          [ HH.text "Enter your initial and final S.G. readings." ]
+      , HH.text "Initial Reading:"
+      , HH.input [ HE.onValueInput (HE.input EnterInitial) ]
+      , HH.br []
+      , HH.text "Final Reading:"
+      , HH.input [ HE.onValueInput (HE.input EnterFinal) ]
+      , HH.br []
+      , HH.text (show state.adjusted)
       ]
+  -- FG = 1.0000 – 0.0044993*RIi + 0.011774*RIf + 0.00027581*RIi² – 0.0012717*RIf² – 0.0000072800*RIi³ + 0.000063293*RIf³
+
+  calc :: Number -> Number -> Number
+  calc init final =
+     1.000 - 0.0044993*init + 0.011774*final + 0.00027581* pow init 2.0 - 0.0012717* pow final 2.0 - 0.0000072800*pow init 3.0 + 0.000063293*pow final 3.0
+
 
   eval :: Query ~> H.ComponentDSL State Query Void m
   eval = case _ of
-    ToggleState next -> do
-      _ <- H.modify (\state -> { on: not state.on })
+    EnterInitial v next -> do
+      _ <- H.modify (\state -> 
+            case (fromString v) of
+              Just x -> state { initial = x, adjusted = calc x state.final }
+              _      -> state)
+      pure next
+
+    EnterFinal v next -> do
+      _ <- H.modify (\state -> 
+            case (fromString v) of
+              Just x -> state { final = x, adjusted = calc state.initial x }
+              _      -> state)
       pure next
